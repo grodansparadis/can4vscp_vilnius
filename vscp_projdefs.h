8a0cc0f4aa81b796e69a24ed025cb53a2e05e916
Ake Hedman <akhe@grodansparadis.com>
2016-03-23 22:07:59 +0100

    Version 1.0.3

--- vscp_projdefs.h
+++ vscp_projdefs.h
@@ -2,7 +2,7 @@
  * 	VSCP (Very Simple Control Protocol) 
  * 	http://www.vscp.org
  *
- *  Copyright (C) 1995-2013 Ake Hedman, Grodans Paradis AB,
+ *  Copyright (C) 1995-2016 Ake Hedman, Grodans Paradis AB,
  *                              <akhe@grodansparadis.com>
  *
  *  This work is licensed under the Creative Common 
@@ -29,11 +29,11 @@
 // Enable error reporting
 #define VSCP_FIRMWARE_ENABLE_ERROR_REPORTING
 
-// Enable write to protected locations
-#define ENABLE_WRITE_2PROTECTED_LOCATIONS 
-
 // Enable logging functionality
 #define VSCP_FIRMWARE_ENABLE_LOGGING
+
+// Enable write to protected locations
+#define ENABLE_WRITE_2PROTECTED_LOCATIONS 
 
 // EEPROM bigger then 256 bytes
 //#define EEADRH
